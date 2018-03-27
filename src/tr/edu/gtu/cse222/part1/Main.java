e97c884394eeedbb4e24ad5ef816a9417806e858
tbb <enesg@protonmail.com>
2018-03-28 00:57:49 +0300

    part 1 main updated

--- src/tr/edu/gtu/cse222/part1/Main.java
+++ src/tr/edu/gtu/cse222/part1/Main.java
@@ -1,7 +1,32 @@
 package tr.edu.gtu.cse222.part1;
 
+import java.util.LinkedList;
+
 public class Main {
+
     public static void main(String[] args) {
+        LinkedList<Course> filtered = null;
+
         CourseStructure cs = new CourseStructure("courses.csv");
+        try {
+            filtered = cs.getByCode("CSE 222");
+        } catch (CourseStructure.CodeNotFoundException e) {
+            e.printStackTrace();
+        }
+        filtered.forEach(System.out::println);
+
+        try {
+            filtered = cs.listSemesterCourses(4);
+        } catch (CourseStructure.SemesterNotFoundException e) {
+            e.printStackTrace();
+        }
+        filtered.forEach(System.out::println);
+
+        try {
+            filtered = cs.getByRange(2, 10);
+        } catch (CourseStructure.EmptyRangeException e) {
+            e.printStackTrace();
+        }
+        filtered.forEach(System.out::println);
     }
 }
